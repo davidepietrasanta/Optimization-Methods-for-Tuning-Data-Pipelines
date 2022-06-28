5691fbf03f27ae28e9403b3e486f005ce4611c6a
davide.pietrasanta <davide.pietrasanta@virgilio.it>
2022-06-28 14:07:25 +0200

    doc: Tutorial updated

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pandas as pd # pylint: disable=unused-import
 from src.config import DATASET_FOLDER_MEDIUM ,DATASET_FOLDER # pylint: disable=unused-import
 from src.config import METAFEATURES_FOLDER, list_of_metafeatures # pylint: disable=unused-import
 from src.config import LIST_OF_ML_MODELS, LIST_OF_PREPROCESSING # pylint: disable=unused-import
-from src.utils.metalearner import data_preparation, train_metalearner, choose_performance_from_metafeatures # pylint: disable=unused-import
+from src.utils.metalearner import data_preparation, train_metalearner # pylint: disable=unused-import
+from src.utils.metalearner import choose_performance_from_metafeatures # pylint: disable=unused-import
 
 if __name__ == '__main__':
     VERBOSE = True
